479b0086af855ca10b77fd0f7c9b26100b897066
ripka <ripka@google.com>
2020-09-06 15:53:06 +0000

    script to create loaded pod

---
+++ apply-load.sh
@@ -0,0 +1,3 @@
+ingress_ip=$(kubectl get service istio-ingress -n gke-system -o jsonpath={.status.loadBalancer.ingress[0].ip}- kubeconfig ~/.kube/cloudrun) 
+sed "s/INGRESS_IP/$ingress_ip/g" hello-app-more-load-job.yaml | kubectl apply -f -
+
