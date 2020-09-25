e1939d09bcc1ea8b973587dc96fa5463bd07cf59
ripka <ripka@google.com>
2020-09-25 16:39:31 +0000

    add cluster exists check

--- scripts/create-asm-cluster.sh
+++ scripts/create-asm-cluster.sh
@@ -1,3 +1,10 @@
+isASMCluster=$(gcloud container clusters list --filter name="${CLUSTER_NAME}" --format='table[no-heading]("NAME")')
+
+if [ isASMCluster ]; then
+	echo "cluster exists already skipping create"
+        exit;
+fi
+
 gcloud beta container clusters create ${CLUSTER_NAME} \
     --zone ${CLUSTER_ZONE} \
     --release-channel=regular \
