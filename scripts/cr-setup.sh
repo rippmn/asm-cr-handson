b64af644594b33234614771c067be2e6db6477d3
ripka <ripka@google.com>
2020-12-16 18:39:20 +0000

    script improvements for 1.7

--- scripts/cr-setup.sh
+++ scripts/cr-setup.sh
@@ -6,22 +6,24 @@ cp ~/.kube/config ~/.kube/cloudrun
 NAMESPACE=default 
 
 ingress_ip=""
-while [ ! $ingress_ip ];
+echo "waiting for ingress to be completed"
+while [[ ! $ingress_ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]];
 do
   sleep 2
   ingress_ip=$(kubectl get service istio-ingress -n gke-system -o jsonpath={.status.loadBalancer.ingress[0].ip} --kubeconfig ~/.kube/cloudrun)
 done
 
-configExists=$(kubectl get configmap config-domain -n knative-serving -o jsonpath={.metadata.name} --kubeconfig ~/.kube/cloudrun)
+domainCheck=$(kubectl get configmap config-domain -n knative-serving -o json | jq -r '.data | keys[0]')
+
 sleep 2
-while [ ! $configExists ];
+echo "updating default domain to $ingress_ip.xip.io"
+while [ ! "$domainCheck" == "${ingress_ip}.xip.io" ];
 do
   sleep 2
-  configExists=$(kubectl get configmap config-domain -n knative-serving -o jsonpath={.metadata.name} --kubeconfig ~/.kube/cloudrun)
+  kubectl patch configmap config-domain --kubeconfig ~/.kube/cloudrun --namespace knative-serving --patch \
+    '{"data": {"example.com": null, "'${ingress_ip}'.xip.io": ""}}'
+  domainCheck=$(kubectl get configmap config-domain -n knative-serving -o json | jq -r '.data | keys[0]')
 done
-
-kubectl patch configmap config-domain --kubeconfig ~/.kube/cloudrun --namespace knative-serving --patch \
-  '{"data": {"example.com": null, "'${ingress_ip}'.xip.io": ""}}'
 #gcloud beta run deploy hello-load-app --namespace default --image  gcr.io/${PROJECT_ID}/load-app:1.0 \
 gcloud beta run deploy hello-load-app --namespace default --image  rippmn/hello-bg-app:0.1 \
 --max-instances 3 --platform gke --cluster=${CLUSTER2_NAME} --cluster-location=${CR_CLUSTER_ZONE} --concurrency=40
