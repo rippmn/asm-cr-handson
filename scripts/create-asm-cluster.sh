fb76d4d442ad3b5f51537a4f9047100d80b4cc7a
Andrew Ripka <ripka@google.com>
2021-11-11 23:13:16 +0000

    update for new cluster creation cli cmd

--- scripts/create-asm-cluster.sh
+++ scripts/create-asm-cluster.sh
@@ -2,7 +2,7 @@ isASMCluster=$(gcloud container clusters list --filter name="${CLUSTER_NAME}" --
 
 if [ $isASMCluster ]; then
 	echo "cluster ${CLUSTER_NAME} already provsioned skipping create"
-        exit;
+       exit;
 fi
 
 gcloud beta container clusters create ${CLUSTER_NAME} \
@@ -11,7 +11,7 @@ gcloud beta container clusters create ${CLUSTER_NAME} \
     --machine-type=e2-standard-4 \
     --num-nodes=4 \
     --workload-pool=${WORKLOAD_POOL} \
-    --enable-stackdriver-kubernetes \
+    --logging=SYSTEM,WORKLOAD \
     --subnetwork=default \
     --scopes cloud-platform \
     --labels mesh_id=${MESH_ID} -q --verbosity none & 
